71988f664a77595d2eb0e3d2afb575b097f119c5
Zicky Deng <dengzicky99@gmail.com>
2020-04-30 17:36:38 -0400

    both files updated

--- 1app.js
+++ 1app.js
@@ -1,4 +1,4 @@
-function randomRGB(){
+function randomRGB() {
     const r = Math.floor(Math.random() * 256);
     const g = Math.floor(Math.random() * 256);
     const b = Math.floor(Math.random() * 256);
@@ -7,8 +7,8 @@ function randomRGB(){
 
 //To change each letter independently
 const letters = document.querySelectorAll('.letter');
-const intervalId = setInterval(function(){
-    for (let letter of letters){
+const intervalId = setInterval(function () {
+    for (let letter of letters) {
         letter.style.color = randomRGB();
     }
 }, 1000);
